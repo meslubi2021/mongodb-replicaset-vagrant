76470d3e56afa5d2291d553f48234b34c517eb68
Tarun Gupta <tarungupta@Taruns-MacBook-Pro.local>
2018-03-31 14:42:34 +0530

    Usernames and password to be passed from environment variables

--- init.sh
+++ init.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+# Inititalizing variables
+readonly INSTALL_DIR="/home/vagrant"
+readonly MONGODB_CONF_FILES=("mongodbPrimary.conf" "mongodbSecondary1.conf" "mongodbSecondary2.conf")
+readonly MONGODB_NODES=("mongodbPrimary" "mongodbSecondary1" "mongodbSecondary2")
 readonly MONGODB_PORTS=(27017 27018 27019)
+readonly MONGODB_ROOT_USER="db_root"
+readonly MONGODB_ROOT_PASSWORD="db@p@ss@root"
 
 # Clear all ports to ensure no application is running
 for i in "${MONGODB_PORTS[@]}"
 do
-mongo --port $i admin --eval 'db.shutdownServer()'
+mongo --port $i admin --eval 'db.auth("'"$MONGODB_ROOT_USER"'", "'"$MONGODB_ROOT_PASSWORD"'"); db.shutdownServer()'
 done
 
 # Run all nodes in mongodb replica set while starting the machine
-mongod --config "~/MongoDB/mongodbPrimary.conf" --fork
-mongod --config "~/MongoDB/mongodSecondary1.conf" --fork
-mongod --config "~/MongoDB/mongodSecondary2.conf" --fork
+for i in "${MONGODB_CONF_FILES[@]}"
+do
+  mongod --config "$INSTALL_DIR/MongoDB/$i" --fork
+done
